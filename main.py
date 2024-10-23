701cfc9c648a97aee8e76815ff12c2e1b0e7b851
Benjamin Briolini <benjamin.briolini@gmail.com>
2024-10-23 20:51:00 -0300

    Interfaces, main y demas

---
+++ main.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+from tkinter import messagebox
+from PIL import Image, ImageTk
+from calculo_de_area import abrir_area
+import sys
+
+ventana = tk.Tk()
+ventana.title("PROGRAMA DE ESTADÍSTICA Y PROBABILIDAD")
+ventana.geometry(f"800x600")
+ventana.configure(bg="#1F6680")
+
+# Logo Isaui
+imagen = Image.open("C:/Users/benja/OneDrive/Desktop/Estadistica/isaui.png")
+imagen_redimensionada = imagen.resize((500, 350))  # Tamaño ajustado
+imagen_logo = ImageTk.PhotoImage(imagen_redimensionada)
+label_imagen = tk.Label(ventana, image=imagen_logo, bg="#1F6680")
+label_imagen.place(relx=0.5, y=100,anchor='center')  # Ajuste de posición
+
+#Labels
+label_estadistica = tk.Label(ventana, text="ESTADÍSTICA Y PROBABILIDAD\nAPLICADA", font=("Arial", 24), bg="#1F6680", fg="white")
+label_estadistica.place(relx=0.5, y=220, anchor='center')
+
+label_pregunta = tk.Label(ventana, text="¿Qué operación desea realizar?" , font=("Arial", 24), bg="#1F6680", fg="white")
+label_pregunta.place(relx=0.5, y=300, anchor='center')
+
+#BOTONES
+boton_area = tk.Button(ventana, text="CÁLCULO DE ÁREA", width=22, fg="White", font=("Arial", 14), bg="#274357",command=abrir_area)
+boton_area.place(relx=0.5, y=370, anchor='center') 
+
+boton_ecuacion = tk.Button(ventana, text="ECUACIONES LINEALES", width=22, fg="White", font=("Arial", 14), bg="#274357")
+boton_ecuacion.place(relx=0.5, y=440, anchor='center') 
+
+boton_salir = tk.Button(ventana, text="SALIR", width=22, fg="White", font=("Arial", 14), bg="#274357", command=sys.exit)
+boton_salir.place(relx=0.5, y=510, anchor='center') 
+
+
+ventana.mainloop()
